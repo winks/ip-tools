3cb7d1dc8d411f29965c8121064060545b3936f9
Florian Anderiasch <fa@art-core.org>
2022-06-01 07:20:23 +0100

    foo

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import {
   type ConnInfo,
   type Handler,
   type ServeInit
-} from "https://deno.land/std@0.125.0/http/server.ts";
+} from "https://deno.land/std@0.141.0/http/server.ts";
 
 // https://stackoverflow.com/questions/71008150/get-remote-client-ip-address-in-deno
 function assertIsNetAddr (addr: Deno.Addr): asserts addr is Deno.NetAddr {
@@ -17,33 +17,50 @@ function getRemoteAddress (connInfo: ConnInfo): Deno.NetAddr {
   return connInfo.remoteAddr;
 }
 
+function getHost (host: String) {
+  var p = host.split(':')
+  if (p.length < 3) {
+    return p[0];
+  } else {
+    return host;
+  }
+}
+
+function fullInfo (clientIp: String, request: any, url: URL) {
+  var body = [];
+
+  const left = 21;
+  const headers = ['user-agent', 'accept', 'accept-language', 'accept-encoding', 'connection'];
+
+  body.push("REMOTE_ADDR".padEnd(left) + `: ${clientIp}`);
+  headers.forEach(el => {
+    body.push(el.padEnd(left) + `: ${request.headers.get(el)}`);
+  });
+  body.push("REQUEST_METHOD".padEnd(left) + `: ${request.method}`);
+  body.push("HTTP_HOST".padEnd(left)      + `: ${getHost(url.host)}`);
+  body.push("SCRIPT_NAME".padEnd(left)    + `: ${url.pathname}`);
+  body.push("REQUEST_URI".padEnd(left)    + `: ${url.pathname}${url.search}`);
+
+  return body.join('\n');
+}
+
 const handler: Handler = (request, connInfo) => {
   const url = new URL(request.url);
   if (url.pathname == '/favicon.ico') {
     return new Response('');
   }
-  const {hostname, port} = getRemoteAddress(connInfo);
-  //console.log(request.headers);
-  console.log(request);
-  //console.log("Path:", url.pathname);
-  //console.log("Query parameters:", url.searchParams);
+  const serverHost = getHost(url.host);
+  const subdomain = serverHost.split('.')[0];
 
-  const hd = [ 'user-agent', 'accept', 'accept-language', 'accept-encoding', 'connection'];
+  const {hostname, port} = getRemoteAddress(connInfo);
+  const clientIp = hostname;
 
   var body = '';
-  const left = 21;
 
-  if (url.pathname == '/i') {
-    body += "REMOTE_ADDR".padEnd(left) + `: ${hostname}\n`;
-    hd.forEach(el => {
-      body += el.padEnd(left) + `: ${request.headers.get(el)}\n`;
-    });
-    body += "REQUEST_METHOD".padEnd(left) + `: ${request.method}\n`;
-    body += "HTTP_HOST".padEnd(left) + `: ${url.host}\n`;
-    body += "SCRIPT_NAME".padEnd(left) + `: ${url.pathname}\n`;
-    body += "REQUEST_URI".padEnd(left) + `: ${url.pathname}${url.search}\n`;
+  if (url.pathname == '/i' || subdomain == 'i2' || subdomain == 'i') {
+    body = fullInfo(clientIp, request, url);
   } else {
-    body = `${hostname}\n`;
+    body = `${clientIp}\n`;
   }
   return new Response(body, {
     headers: { "content-type": "text/plain" },
